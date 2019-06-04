a8dc0ab67438779f4c76e0b9733504988e70ab4a
Vishnu <vishnumurali7000@gmail.com>
2019-06-04 20:55:46 +0530

    Changes made

--- src/main.c
+++ src/main.c
@@ -7,29 +7,29 @@ void printCXString(CXString s)
   clang_disposeString(s);
 }
 
-void get_cursor_type(CXCursor cursor){
+void print_cursor_type(CXCursor cursor){
   CXType cursor_type = clang_getCursorType(cursor);
   printCXString(clang_getTypeSpelling(cursor_type));
 }
 
-void get_cursor_spelling(CXCursor cursor){
+void print_cursor_spelling(CXCursor cursor){
   CXString cursor_spelling = clang_getCursorSpelling(cursor);
   printCXString(cursor_spelling);
 }
 
- enum CXChildVisitResult enum_visitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
+enum CXChildVisitResult enum_visitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
   if(clang_getCursorKind(cursor) == CXCursor_IntegerLiteral){
     long long enum_const_value = clang_getEnumConstantDeclValue(parent);
     printf("IntegerLiteral\t %lld \t\tParent = ", enum_const_value);
-    get_cursor_spelling(parent);
+    print_cursor_spelling(parent);
     printf("\n");
   }
 
   else if(clang_getCursorKind(cursor) == CXCursor_EnumConstantDecl){
     printf("EnumConstantDecl\t");
-    get_cursor_spelling(cursor);
+    print_cursor_spelling(cursor);
     printf("\tParent = ");
-    get_cursor_spelling(parent);
+    print_cursor_spelling(parent);
     printf("\n");
   }
   return CXChildVisit_Recurse;
@@ -37,7 +37,7 @@ void get_cursor_spelling(CXCursor cursor){
 
 void enum_handler(CXCursor cursor){
   printf("EnumDecl\t");
-  get_cursor_spelling(cursor);
+  print_cursor_spelling(cursor);
   printf("\n");
   clang_visitChildren(cursor, enum_visitor, 0);
 }

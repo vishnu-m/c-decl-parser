ff9d3a873fcef34c0090bfff2e523ef462d6a3dc
Vishnu <vishnumurali7000@gmail.com>
2019-06-10 18:27:54 +0530

    Clang-Struct declaration

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <clang-c/Index.h>
 #include <stdio.h>
 
-void printCXString(CXString s)
+enum CXChildVisitResult visitor(CXCursor, CXCursor, CXClientData); 
+enum CXChildVisitResult enum_visitor(CXCursor, CXCursor, CXClientData); 
+enum CXChildVisitResult struct_visitor(CXCursor, CXCursor, CXClientData); 
+void enum_handler(CXCursor);
+void struct_handler(CXCursor);
+
+
+void print_CXString(CXString s)
 {
   printf("%s", clang_getCString(s));
   clang_disposeString(s);
@@ -9,18 +16,46 @@ void printCXString(CXString s)
 
 void print_cursor_type(CXCursor cursor){
   CXType cursor_type = clang_getCursorType(cursor);
-  printCXString(clang_getTypeSpelling(cursor_type));
+  print_CXString(clang_getTypeSpelling(cursor_type));
 }
 
 void print_cursor_spelling(CXCursor cursor){
   CXString cursor_spelling = clang_getCursorSpelling(cursor);
-  printCXString(cursor_spelling);
+  print_CXString(cursor_spelling);
+}
+
+void print_cursor_kind(CXCursor cursor){
+  enum CXCursorKind cursor_kind = clang_getCursorKind(cursor);
+  print_CXString(clang_getCursorKindSpelling(cursor_kind));
+}
+
+void print_parent(CXCursor cursor){
+   CXCursor parent = clang_getCursorLexicalParent(cursor);
+   print_CXString(clang_getCursorDisplayName(parent));
+}
+
+void enum_handler(CXCursor cursor){
+  printf("EnumDecl\t");
+  print_cursor_spelling(cursor);
+  printf("\t\tParent = ");
+  print_parent(cursor);
+  printf("\n");
+  clang_visitChildren(cursor, enum_visitor, 0);
+}
+
+void struct_handler(CXCursor cursor){
+  printf("StructDecl\t");
+  print_cursor_spelling(cursor);
+  printf("\t\tParent = ");
+  print_parent(cursor);
+  printf("\n");
+  clang_visitChildren(cursor, struct_visitor, 0);
 }
 
 enum CXChildVisitResult enum_visitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
   if(clang_getCursorKind(cursor) == CXCursor_IntegerLiteral){
     long long enum_const_value = clang_getEnumConstantDeclValue(parent);
-    printf("IntegerLiteral\t %lld \t\tParent = ", enum_const_value);
+    printf("IntegerLiteral\t\t value = %lld \t\tParent = ", enum_const_value);
     print_cursor_spelling(parent);
     printf("\n");
   }
@@ -28,23 +63,45 @@ enum CXChildVisitResult enum_visitor(CXCursor cursor, CXCursor parent, CXClientD
   else if(clang_getCursorKind(cursor) == CXCursor_EnumConstantDecl){
     printf("EnumConstantDecl\t");
     print_cursor_spelling(cursor);
-    printf("\tParent = ");
+    printf("\t\tParent = ");
     print_cursor_spelling(parent);
     printf("\n");
   }
   return CXChildVisit_Recurse;
 }
 
-void enum_handler(CXCursor cursor){
-  printf("EnumDecl\t");
-  print_cursor_spelling(cursor);
-  printf("\n");
-  clang_visitChildren(cursor, enum_visitor, 0);
+enum CXChildVisitResult struct_visitor(CXCursor cursor, CXCursor parent, CXClientData client_data){
+  // print_cursor_kind(cursor);
+  // printf("\t");
+  // print_cursor_spelling(cursor);
+  // printf("\n");
+  if(clang_getCursorKind(cursor) == CXCursor_FieldDecl){
+    printf("FieldDecl\t");
+    print_cursor_spelling(cursor);
+    printf("\t\ttype = ");
+    print_cursor_type(cursor);
+    printf("\t\tParent = ");
+    print_cursor_spelling(parent);
+    printf("\n");
+  }
+
+  else if(clang_getCursorKind(cursor) == CXCursor_StructDecl){
+    struct_handler(cursor);
+  }
+
+  else if(clang_getCursorKind(cursor) == CXCursor_EnumDecl){
+    enum_handler(cursor);
+  }
+  
+  return CXChildVisit_Continue;
 }
 
 enum CXChildVisitResult visitor(CXCursor cursor, CXCursor parent, CXClientData client_data) {
   if(clang_getCursorKind(cursor) == CXCursor_EnumDecl){
     enum_handler(cursor);
+  }
+  else if(clang_getCursorKind(cursor) == CXCursor_StructDecl){
+    struct_handler(cursor);
   }
   return CXChildVisit_Continue;
 }
--- src/test.c
+++ src/test.c
@@ -1,2 +1,15 @@
-enum week{Mon=99, Tue, Wed, Thur, Fri, Sat, Sun};
+// enum week{Mon=99, Tue, Wed, Thur, Fri, Sat, Sun};
+
+struct Student
+{
+    char name[50];
+    int age;
+    struct marks{
+        int physics;        
+    }m1, m2;
+    struct Student *pointer;
+    enum Fruit{Apple, Banana=23};
+    
+};
+
 enum colours{Red, Black, Yellow};

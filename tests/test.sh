7b386993d67728c91fe9cb0e293e4cb02ab55ed9
Vishnu M <vishnumurali7000@gmail.com>
2019-06-19 19:19:30 +0530

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@
 for file in $PWD/*.c; do
     out=${file%.c}.out
     tmp=`mktemp /tmp/test.XXXXXX`
-    make curr_file=$file specific_file_out | grep -vE '^make|^./libclang_parse.bin' >> $tmp
+    make curr_file=$file specific_file_out | grep -vE '^make|^./libclang_parse.bin' > $tmp
     if ! cmp --silent $tmp $out; then
         echo "test failed: $file"
         exit 1
